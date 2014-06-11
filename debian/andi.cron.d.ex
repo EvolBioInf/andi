8d2e0fea5ba960fd32b5d676008b2e7eb298b6a0
Fabian Klötzl <kloetzl@evolbio.mpg.de>
2014-06-11 12:55:26 +0200

    fixes various files for the debian package

--- debian/andi.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the andi package
-#
-0 4	* * *	root	[ -x /usr/bin/andi_maintenance ] && /usr/bin/andi_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(andi):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="andi" command="/usr/bin/andi"

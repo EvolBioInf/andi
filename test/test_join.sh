0c3afc69b2f3db29d06ed40807d1ce8055f2b2ec
Fabian Klötzl <fabian@kloetzl.info>
2015-01-25 17:32:33 +0100

    fixes join test

--- test/test_join.sh
+++ test/test_join.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -f
 
-./test/test_fasta -l 1000 -L 1000 -k 0.1 > p1.fasta
-./test/test_fasta -l 1000 -L 1000 -k 0.1 > p2.fasta
+./test/test_fasta -l 1000 -L 1000 -d 0.1 > p1.fasta
+./test/test_fasta -l 1000 -L 1000 -d 0.1 > p2.fasta
 
-head -n 2 p1.fasta p2.fasta > S1.fasta
-tail -n 2 p1.fasta p2.fasta > S2.fasta
+head -qn 2 p1.fasta p2.fasta > S0.fasta
+tail -qn 2 p1.fasta p2.fasta > S1.fasta
 
 rm p1.fasta p2.fasta;
 
-RES=$($srcdir/src/andi -r -j S1.fasta S2.fasta |
+RES=$($srcdir/src/andi -r -j S0.fasta S1.fasta |
 	tail -n 1 |
 	awk '{print ($2 - 0.1)}' |
 	awk 'function abs(x){return ((x < 0.0) ? -x : x)} {print abs($1-$2) < 0.01}'

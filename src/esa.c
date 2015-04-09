EvolBioInf/andi

commit 72b59da607062c1c4711f68e99446dff475df9bf

simplifies code

src/esa.c
@@ -478,17 +478,15 @@ static lcp_inter_t *get_interval( const esa_t *C, lcp_inter_t *ij, char a){
 		SoSueMe:
 		if( c == a){
 			/* found ! */
-			saidx_t n;
-			if( LCP[i] <= LCP[m]){
-				n = L(CLD, m);
-			} else {
-				n = R(CLD,i);
-			}
+			saidx_t n = L(CLD, m);
 
-			ij->i = i;
-			ij->j = m-1;
-			ij->m = n;
-			ij->l = LCP[n];
+			*ij = (seq_t){
+				.i = i,
+				.j = m-1,
+				.m = n,
+				.l = LCP[n]
+			};
+
 			return ij;
 		}
 

commit 8a140e0e3f04622c4853cc6fa0813f1518c62c2d

fixes bug introduced in 72b59da6

src/esa.c
@@ -480,7 +480,7 @@ static lcp_inter_t *get_interval( const esa_t *C, lcp_inter_t *ij, char a){
 			/* found ! */
 			saidx_t n = L(CLD, m);
 
-			*ij = (seq_t){
+			*ij = (lcp_inter_t){
 				.i = i,
 				.j = m-1,
 				.m = n,
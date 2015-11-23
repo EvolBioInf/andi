489cc9a646df54db2844120714a7fbcac67778d4
Fabian Klötzl <fabian@kloetzl.info>
2015-11-23 10:28:41 +0100

    fixes join test

--- test/test_join.sh
+++ test/test_join.sh
@@ -13,7 +13,7 @@ tail -qn 2 p1.fasta p2.fasta p3.fasta > S1.fasta
 rm p1.fasta p2.fasta p3.fasta;
 
 
-RES=$($srcdir/src/andi -rt 1 -j S0.fasta S1.fasta |
+RES=$(./src/andi -rt 1 -j S0.fasta S1.fasta |
 	tail -n 1 |
 	awk '{print ($2 - 0.1)}' |
 	awk 'function abs(x){return ((x < 0.0) ? -x : x)} {print abs($1-$2) < 0.01}'
@@ -35,7 +35,7 @@ tail -qn 2 p2.fasta p3.fasta > S1.fasta
 rm p2.fasta p3.fasta;
 
 
-RES=$($srcdir/src/andi -rt1 -j S0.fasta S1.fasta |
+RES=$(./src/andi -rt1 -j S0.fasta S1.fasta |
         tail -n 1 |
         awk '{print ($2 - 0.1)}' |
         awk 'function abs(x){return ((x < 0.0) ? -x : x)} {print abs($1-$2) < 0.01}'
@@ -59,7 +59,7 @@ tail -qn 2 p1.fasta p2.fasta p3.fasta > S1.fasta
 rm p1.fasta p2.fasta p3.fasta;
 
 
-RES=$($srcdir/src/andi -rt 1 -j S0.fasta S1.fasta |
+RES=$(./src/andi -rt 1 -j S0.fasta S1.fasta |
         tail -n 1 |
         awk '{print ($2 - 0.1)}' |
         awk 'function abs(x){return ((x < 0.0) ? -x : x)} {print abs($1-$2) < 0.01}'

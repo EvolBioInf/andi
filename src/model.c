761857e998806e863c8bebf24577b40d3c78a874
Fabian Klötzl <fabian@kloetzl.info>
2016-02-01 11:53:09 +0100

    simplifies model_count*

--- src/model.c
+++ src/model.c
@@ -154,31 +154,39 @@ model model_bootstrap(const model MM) {
 	return datum;
 }
 
-#define unlikely(x) __builtin_expect((x), 0)
+/**
+ * @brief Given an anchor, classify nucleotides.
+ *
+ * For anchors we already know that the nucleotides of the subject and the query
+ * are equal. Thus only one sequence has to be analysed. See `model_count` for
+ * an explanation of the algorithm.
+ *
+ * @param MM - The mutation matrix
+ * @param S - The subject
+ * @param len - The anchor length
+ */
 void model_count_equal(model *MM, const char *S, size_t len) {
 	size_t local_counts[4] = {0};
+
 	for (; len--;) {
 		char s = *S++;
 
-		// Skip special characters.
-		if (unlikely(s < 'A')) {
+		if (s < 'A') {
 			continue;
 		}
 
 		unsigned char nibble_s = s & 7;
-		unsigned char foo;
 
 		static const unsigned int mm1 = 0x20031000;
 
-		foo = (mm1 >> (4 * nibble_s)) & 0x3;
-		local_counts[foo]++;
+		unsigned char index = (mm1 >> (4 * nibble_s)) & 0x3;
+		local_counts[index]++;
 	}
 
-	for (int i = 0; i != 4; ++i) {
-		static const unsigned int map4 = 0x9740;
-		unsigned int base = (map4 >> (4 * i)) & 0xf;
-		MM->counts[base] += local_counts[i];
-	}
+	MM->counts[AtoA] += local_counts[0];
+	MM->counts[CtoC] += local_counts[1];
+	MM->counts[GtoG] += local_counts[2];
+	MM->counts[TtoT] += local_counts[3];
 }
 
 /**
@@ -191,48 +199,54 @@ void model_count_equal(model *MM, const char *S, size_t len) {
  */
 void model_count(model *MM, const char *S, const char *Q, size_t len) {
 	size_t local_counts[MUTCOUNTS] = {0};
-	size_t j = 0;
-	for (; len > 0; len -= j) {
-		uint8_t u8counts[MUTCOUNTS] = {0};
 
-		j = 255;
-		if (len < 255) j = len;
+	for (; len--; S++, Q++) {
+		char s = *S;
+		char q = *Q;
 
-		for (size_t jj = j; jj--; S++, Q++) {
-			char s = *S;
-			char q = *Q;
-
-			// Skip special characters.
-			if (unlikely(s < 'A' || q < 'A')) {
-				continue;
-			}
-
-			unsigned char nibble_s = s & 7;
-			unsigned char nibble_q = q & 7;
-
-			static const unsigned int mm1 = 0x20031000;
-
-			unsigned char foo = (mm1 >> (4 * nibble_s)) & 0x3;
-			unsigned char baz = (mm1 >> (4 * nibble_q)) & 0x3;
-
-			if (baz > foo) {
-				int temp = foo;
-				foo = baz;
-				baz = temp;
-			}
-
-			static const unsigned int map4 = 0x9740;
-			unsigned int base = (map4 >> (4 * baz)) & 0xf;
-			unsigned int index = base + (foo - baz);
-
-			// unsigned char index = (bar >> ((mm1 >> (4*nibble_q)) & 0x3)) &
-			// 0xf;
-			u8counts[index]++;
+		// Skip special characters.
+		if (s < 'A' || q < 'A') {
+			continue;
 		}
 
-		for (int i = 0; i != MUTCOUNTS; ++i) {
-			local_counts[i] += u8counts[i];
+		/* We want to map characters:
+		 *  A → 0
+		 *  C → 1
+		 *  G → 2
+		 *  T → 3
+		 * The trick used below is that the three lower bits of the
+		 * characters are unique. Thus, they can be used to compute the mapping
+		 * above. The mapping itself is done via tricky bitwise operations.
+		 */
+
+		unsigned char nibble_s = s & 7;
+		unsigned char nibble_q = q & 7;
+
+		static const unsigned int mm1 = 0x20031000;
+
+		// Pick the correct two bits representing s and q.
+		unsigned char foo = (mm1 >> (4 * nibble_s)) & 0x3;
+		unsigned char baz = (mm1 >> (4 * nibble_q)) & 0x3;
+
+		/*
+		 * The mutation matrix is symmetric. For convenience we define the
+		 * mutation XtoY with X > Y.
+		 */
+		if (baz > foo) {
+			int temp = foo;
+			foo = baz;
+			baz = temp;
 		}
+
+		/*
+		 * Finally, we want to map the indices to the correct mutation. This is
+		 * done by utilising the mutation types in model.h.
+		 */
+		static const unsigned int map4 = 0x9740;
+		unsigned int base = (map4 >> (4 * baz)) & 0xf;
+		unsigned int index = base + (foo - baz);
+
+		local_counts[index]++;
 	}
 
 	for (int i = 0; i != MUTCOUNTS; ++i) {

3e2c90d66165869b91d9542e6ad682ef1642b87a
Fabian Klötzl <fabian@kloetzl.info>
2016-02-13 09:20:00 +0100

    adds missing include
    This fixes issue #6

--- opt/strchrnul.c
+++ opt/strchrnul.c
@@ -1,6 +1,8 @@
 /* @brief Here follows a simple implementation of the GNU function `strchrnul`.
  * Please check the gnulib manual for details.
  */
+#include <string.h>
+
 char *strchrnul(const char *s, int c){
 	char *p = strchr(s,c);
 
